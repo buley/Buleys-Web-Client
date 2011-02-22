5bfb0634192b3ef3e2141ad16b408aa7e00e15f8
Taylor Buley <tbuley@forbes.com>
2011-02-21 20:08:31 -0800

    Makesprite.png script for OS X

--- images/makesprite.sh
+++ images/makesprite.sh
@@ -3,7 +3,7 @@ sudo /ImageMagick-6.6.7/bin/convert 1percenttransparency.png 2percenttransparenc
 sudo /ImageMagick-6.6.7/bin/convert heart.png heart-empty.png star.png star-empty.png +append actions_1.png
 sudo /ImageMagick-6.6.7/bin/convert mail.png mail-send.png inbox.png inbox-document.png +append actions_2.png
 sudo /ImageMagick-6.6.7/bin/convert thumb.png thumb-empty.png thumb-up.png thumb-up-empty.png +append actions_3.png
-sudo /ImageMagick-6.6.7/bin/convert cross-button.png question.png transparencies.png +append actions_4.png
+sudo /ImageMagick-6.6.7/bin/convert cross-button.png question.png +append actions_4.png
 sudo /ImageMagick-6.6.7/bin/convert 154.png 158.png 159.png 160.png +append circles.png
 sudo /ImageMagick-6.6.7/bin/convert facebook.png twitter.png reddit.png digg.png +append social.png
 sudo /ImageMagick-6.6.7/bin/convert actions_1.png actions_2.png actions_3.png circles.png social.png actions_4.png -append sprite.png
