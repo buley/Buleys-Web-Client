f0333c6d9414ed45d2e49cae1086459aedcc10d4
Taylor Buley <tbuley@forbes.com>
2011-09-02 22:26:21 -0400

    read.js now indb

--- js/core/development/read.dev.js
+++ js/core/development/read.dev.js
@@ -1,232 +1,152 @@
 
-	function get_read( type_filter, slug_filter, begin_timeframe, end_timeframe, make_inverse ) {
+function get_read( type_filter, slug_filter, begin_timeframe, end_timeframe, make_inverse ) {
+
+	/* Action */
+
 	jQuery(document).trigger('get_read');
-	
-	    if (typeof make_inverse == "undefined") {
-	        make_inverse = false;
-	    }
-	
-	    var begin_date = 0;
-	    if (typeof begin_timeframe == "undefined" || begin_timeframe == null) {
-	        begin_date = 0
-	    } else {
-	        begin_date = parseInt(begin_timeframe);
-	    }
-	
-	    var end_date = 0;
-	    if (typeof end_timeframe == "undefined" || end_timeframe == null) {
-	        end_date = new Date().getTime();
-	    } else {
-	        end_date = parseInt(end_timeframe);
-	    }
-	
-	    new_categories_transaction();
-	
-	    Buleys.index = Buleys.objectStoreCategories.index("slug");
-	        var keyRange = IDBKeyRange.only(slug_filter);
-	    var cursorRequest = Buleys.index.openCursor(keyRange);
-	
-	    cursorRequest.onsuccess = function ( event ) {
 
-	
-	        var objectCursor = cursorRequest.result;
-	        if (!objectCursor) {
-	            return;
-	        }
-	
-	
-	
-	        if (objectCursor.length > 1) {
-	            jQuery.each(objectCursor, function ( k, item ) {
+	/* Defaults */
 
-	
-	
-	
-	
-	
-	
-	                new_status_transaction();
-	
-	                var item_request_2 = Buleys.objectStore.get(item.link);
-	                item_request_2.onsuccess = function ( event ) {
-
-	
-	                    if (typeof item_request_2.result !== 'undefined' && make_inverse !== true) {
-	
-	
-	                        if (jQuery("#" + item.link.replace(/[^a-zA-Z0-9-_]+/g, "")).length <= 0) {
-	                            get_item_raw(item_request_2.result.link);
-	                        }
-	
-	                    } else if (typeof item_request_2.result == 'undefined' && make_inverse == true) {
-	
-	
-	                        if (jQuery("#" + item.link.replace(/[^a-zA-Z0-9-_]+/g, "")).length <= 0) {
-	                            get_item_raw(item.link);
-	                        }
-	
-	                    }
-	
-	                };
-	
-	
-	
-	
-	
-	
-	
-	
-	            });
-	
-	        } else {
-	            get_item(objectCursor.link);
-	        }
-	    };
-	    cursorRequest.onerror = function ( event ) {
-
-	
-	    };
-	
-	
-	
+	if (typeof make_inverse == "undefined") {
+		make_inverse = false;
 	}
+
+	/* Setup */
+
+	var begin_date = 0;
+	var end_date = 0;
+
+	if (typeof begin_timeframe == "undefined" || begin_timeframe == null) {
+		begin_date = 0
+	} else {
+		begin_date = parseInt(begin_timeframe);
+	}
+
+	if (typeof end_timeframe == "undefined" || end_timeframe == null) {
+		end_date = new Date().getTime();
+	} else {
+		end_date = parseInt(end_timeframe);
+	}
+
+	/* Callbacks */
+
+	cursor_on_success = function ( context_1 ) {
+
+		var event_1 = context_1.event;
+
+		if( !InDB.isEmpty( InDB.cursor.value( event_1 ) ) ) { 
+
+			if ( make_inverse !== true) {
+				if (jQuery("#" + item.link.replace(/[^a-zA-Z0-9-_]+/g, "")).length <= 0) {
+					get_item_raw(item_request_2.result.link);
+				}
+			} else { 
+				if (jQuery("#" + item.link.replace(/[^a-zA-Z0-9-_]+/g, "")).length <= 0) {
+					get_item_raw(item.link);
+				}
+			}
+		}
+
+	};
+
+	cursor_on_error = function ( event ) {
+
+	};
+
+	/* Request */
 	
-	
-	
-	
-	
-	function mark_item_as_read( item_url, item_slug, item_type ) {
+	InDB.trigger( 'InDB_do_cursor_get', { 'store': 'categories', 'index': 'slug', 'keyRange': InDB.range.only( slug_filter ), 'on_success': cursor_on_success, 'on_error': cursor_on_error } );
+
+}
+
+
+function mark_item_as_read( item_url, item_slug, item_type ) {
+
+	/* Action */
+
 	jQuery(document).trigger('mark_item_as_read');
+	
+	/* Setup */
 
-	
-	
-	    new_status_transaction();
-	
-	    var data = {
-	        "link": item_url,
-	        "topic_slug": item_slug,
-	        "topic_type": item_type,
-	        "modified": new Date().getTime()
-	    };
-	
-	
-	    var add_data_request = Buleys.objectStore.add(data);
-	    add_data_request.onsuccess = function ( event ) {
-
-	
-	        Buleys.objectId = add_data_request.result;
-	    };
-	    add_data_request.onerror = function ( e ) {
-
-	
-	
-	    };
-	
-	}
-	
-	function new_status_transaction(  ) {
-	jQuery(document).trigger('new_status_transaction');
-
-	    try {
-	        var transaction = Buleys.db.transaction(["status"], IDBTransaction.READ_WRITE /*Read-Write*/ , 1000 /*Time out in ms*/ );
-	        transaction.oncomplete = function ( e ) {
-
-	
-	            delete Buleys.objectStore;
-	        };
-	        transaction.onabort = function ( e ) {
-
-	
-	        };
-	        Buleys.objectStore = transaction.objectStore("status");
-	
-	    } catch (e) {
-	
-	
-	
-	
-	        var request = Buleys.db.setVersion(parseInt(Buleys.version, 10 ));
-	        request.onsuccess = function ( e ) {
-
-	
-	            Buleys.objectStore = Buleys.db.createObjectStore("status", {
-	                "keyPath": "link"
-	            }, true);
-	
-	            Buleys.objectStore.createIndex("topic_slug", "topic_slug", {
-	                unique: false
-	            });
-	            Buleys.objectStore.createIndex("topic_type", "topic_type", {
-	                unique: false
-	            });
-	            Buleys.objectStore.createIndex("modified", "modified", {
-	                unique: false
-	            });
-	
-	
-	
-	        };
-	        request.onerror = function ( e ) {
-
-	
-	        };
-	
-	    };
+	var data = {
+		"link": item_url,
+		"topic_slug": item_slug,
+		"topic_type": item_type,
+		"modified": new Date().getTime()
 	}
 
+	/* Callbacks */
+
+	var add_on_success = function ( event ) {
+
+	};
+
+	var add_on_error = function ( e ) {
+
+	};
 	
-	function remove_item_from_read_database( item_url, item_slug, item_type ) {
+	/* Request */
+
+	InDB.trigger( 'InDB_do_row_put', { 'store': 'status', 'key': item_url, 'data': data, 'on_success': add_on_success, 'on_error': add_on_error } );
+
+};
+
+
+function remove_item_from_read_database( item_url, item_slug, item_type ) {
+
+	/* Action */
+	
 	jQuery(document).trigger('remove_item_from_read_database');
 
+	/* Callbacks */
+
+	var add_on_success = function ( event ) {
+
+	};
+
+	var add_on_error = function ( e ) {
+
+	};
 	
-	
-	    new_status_transaction();
-	
-	
-	    var request = Buleys.objectStore["delete"](item_url);
-	    request.onsuccess = function ( event ) {
+	/* Request */
+
+	InDB.trigger( 'InDB_do_row_delete', { 'store': 'status', 'key': item_url, 'on_success': add_on_success, 'on_error': add_on_error } );
+
+}
 
 	
-	        delete Buleys.objectId;
-	    };
-	    request.onerror = function (  ) {
+function add_item_to_readstatus_database( item, status ) {
 
-	
-	    };
-	
-	
-	}
-	
-		
-	function add_item_to_readstatus_database( item, status ) {
+	/* Action */
+
 	jQuery(document).trigger('add_item_to_readstatus_database');
 
-	
-	    if (typeof status == 'undefined') {
-	        status = "unread";
-	    }
-	
-	    new_status_transaction();
-	
-	    var data = {
-	        "link": item.link,
-	        "status": status,
-	        "modified": new Date().getTime()
-	    };
-	
-	
-	    var add_data_request = Buleys.objectStore.add(data);
-	    add_data_request.onsuccess = function ( event ) {
+	/* Defaults */
 
-	
-	        Buleys.objectId = add_data_request.result;
-	    };
-	    add_data_request.onerror = function ( e ) {
-
-	
-	
-	
-	    };
-	
+	if (typeof status == 'undefined') {
+		status = "unread";
 	}
 
+	/* Setup */
+
+	var data = {
+		"link": item.link,
+		"status": status,
+		"modified": new Date().getTime()
+	};
+
+	/* Callbacks */
+
+	var add_on_success = function ( event ) {
+
+	};
+
+	var add_on_error = function ( e ) {
+
+	};
+	
+	/* Request */
+
+	InDB.trigger( 'InDB_do_row_add', { 'store': 'status', 'data': data, 'on_success': add_on_success, 'on_error': add_on_error } );
+
+}
