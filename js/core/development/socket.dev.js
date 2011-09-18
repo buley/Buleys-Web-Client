cc2f67ceb9b969afa7e28365e389fd75c6eaa279
Taylor Buley <tbuley@forbes.com>
2011-09-17 23:25:11 -0400

    tweaking socket

--- js/core/development/socket.dev.js
+++ js/core/development/socket.dev.js
@@ -1,11 +1,7 @@
-/** 
- * Socket.js
- **/
+Buleys.socket = Buleys.socket || {};
+Buleys.websocket = Buleys.websocket || {};
 
-Buleys.socket = Buleys.socket = {};
-Buleys.websocket = Buleys.websocket = {};
-
-Buleys.socket = io.connect('http://api.buleys.com');
+Buleys.socket = io.connecit('http://api.buleys.com');
 
 Buleys.socket.on('connect',function() {
 	Buleys.websocket.login();
@@ -23,27 +19,24 @@ Buleys.socket.on( 'authorized', function( data ) {
 Buleys.socket.on( 'data', function( response ) {
 
 	console.log('DATA', response );
-	//response = jQuery.parseJSON( response );
-	console.log('JSON!!', response.type, response );
 
-	if( 'news' === response.type ) {
-		console.log( 'news!!', response.data );
-		if( 'undefined' !== typeof response.data ) {
-			console.log('adding item data', response.data);
-			add_item_to_items_database( response.data );
-		} else {
-			console.log('undefined, yo', response);
-			add_item_to_items_database( response );
-		}
-	} else if( 'tweet' === response.type ) {
-		console.log( 'tweet', response.data.user, response.data.text );
-		add_tweet_to_items_database( response.data );
+	if( 'tweet' === response.object.object_type || 'news' === response.object.object_type ) {
+
+		var on_success = function( activity_id ) {
+			console.log( 'Buleys.socket.on data success', activity_id );
+		};
+	
+		var on_error = function( error ) {
+			console.log( 'Buleys.socket.on data error', error );
+		};
+
+		Buleys.activity.add( response, on_success, on_error );
 	} 
 	
-});
+} );
 
 
-Buleys.websocket.login = function ( ) {
+Buleys.websocket.login = function() {
 
 	var oauth_access_token = get_local_storage( 'twitter_oauth_access_token' );
 	var oauth_access_secret = get_local_storage( 'twitter_oauth_access_token_secret' );
@@ -51,4 +44,3 @@ Buleys.websocket.login = function ( ) {
 	Buleys.socket.emit( 'connect', { 'topics': [ 'place_chicago', 'place_milwaukee', 'company_csco', 'company_jnpr', 'company_ebay', 'place_miami', 'place_los_angeles', 'place_hollywood', 'place_sacramento', 'company_amzn', 'company_msft', 'company_goog', 'company_aapl', 'company_nflx', 'company_yhoo', 'company_aol', 'place_new_york', 'place_san_francisco', 'place_austin', 'place_nashville' ], 'twitter_enabled': true, 'twitter_access_token': oauth_access_token, 'twitter_access_token_secret': oauth_access_secret } );
 	
 };
-
